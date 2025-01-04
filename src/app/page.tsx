af88b6a911fa399da87ef732570f0f6b2e7cb3d5
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2025-01-04 20:04:04 +0700

    adding sidebar and navbar components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hello
-      </main>
+      <Dashboard/>
   );
 }
